fa540aaf9cd0079754f706761d9df399bd7c40ec
yathirajulasivasankar <yathirajula.sivasankar@gmail.com>
2021-02-05 12:13:12 +0530

    Add files via upload

---
+++ Raknas.framework/Headers/Raknas.h
@@ -0,0 +1,18 @@
+//
+//  Raknas.h
+//  Raknas
+//
+//  Created by SIVASANKAR YATHIRAJULA on 05/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Raknas.
+FOUNDATION_EXPORT double RaknasVersionNumber;
+
+//! Project version string for Raknas.
+FOUNDATION_EXPORT const unsigned char RaknasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Raknas/PublicHeader.h>
+
+#import "RaknasFamily.h"
---
+++ Raknas.framework/Headers/RaknasFamily.h
@@ -0,0 +1,16 @@
+//
+//  RaknasFamily.h
+//  Raknas
+//
+//  Created by SIVASANKAR YATHIRAJULA on 05/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RaknasFamily : NSObject
+-(NSString*)getMyFatherName;
+@end
+
+NS_ASSUME_NONNULL_END
